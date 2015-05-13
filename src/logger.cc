794fa98d8b733b5a210e62f91380b8d592cc823b
Zack Mullaly <redwire@riseup.net>
2015-05-13 13:24:43 -0230

    Implemented a nice colorful logger

---
+++ src/logger.cc
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+
+#include "src/logger.h"
+
+// Standard constructor
+// Threshold adopts a default level of DEBUG if an invalid threshold is provided.
+Logger::Logger(log_level_t threshold)
+{
+  if (threshold < SILLY || threshold > ERROR) {
+    this->threshold = default_log_level;
+  } else {
+    this->threshold = threshold;
+  }
+  log_to_stdout = true;
+  log_to_file = false;
+  log_filename = "";
+}
+
+// Standard destructor
+Logger::~Logger()
+{
+  if (log_file.is_open()) {
+    log_file.close();
+  }
+}
+
+// Configure the logger to log to stdout and/or to a file.
+void Logger::config(bool log_stdout, bool log_to_file, std::string fname)
+{
+  log_to_stdout = log_stdout;
+  this->log_to_file = log_to_file;
+  if (log_to_file) {
+    log_filename = fname;
+    log_file.open(log_filename, std::ios::out | std::ios::app);
+  }
+}
+
+// Update the logger's threshold.
+// If an invalid level is provided, do not update.
+void Logger::set_threshold(log_level_t level)
+{
+  if (level >= SILLY && level <= ERROR) {
+    threshold = level;
+  }
+}
+
+// Standard log function. Prints nice colors for each level.
+void Logger::log(log_level_t level, std::string msg)
+{
+  if (level < SILLY || level > ERROR || level < threshold) {
+    return;
+  }
+  switch (level) {
+  case SILLY:
+    msg = "\033[1;35;47m[SILLY] " + msg + "\033[0m";
+    break;
+  case DEBUG:
+    msg = "\033[1;32m[DEBUG]\033[0m " + msg;
+    break;
+  case VERBOSE:
+    msg = "\033[1;37m[VERBOSE]\033[0m " + msg;
+    break;
+  case INFO:
+    msg = "\033[1;34m[INFO]\033[0m " + msg;
+    break;
+  case WARN:
+    msg = "\033[90;103m[WARN] " + msg + "\033[0m";
+    break;
+  case ERROR:
+    msg = "\033[91;40m[ERROR] " + msg + "\033[0m";
+    break;
+  }
+  if (log_to_stdout) {
+    std::cout << msg << std::endl;
+  }
+  if (log_to_file && log_file.is_open()) {
+    log_file << msg << std::endl;
+  }
+}
+
+// Convenience methods
+
+void Logger::silly(std::string msg)
+{
+  log(SILLY, msg);
+}
+
+void Logger::debug(std::string msg)
+{
+  log(DEBUG, msg);
+}
+
+void Logger::verbose(std::string msg)
+{
+  log(VERBOSE, msg);
+}
+
+void Logger::info(std::string msg)
+{
+  log(INFO, msg);
+}
+
+void Logger::warn(std::string msg)
+{
+  log(WARN, msg);
+}
+
+void Logger::error(std::string msg)
+{
+  log(ERROR, msg);
+}
--- src/logger.h
+++ src/logger.h
@@ -16,7 +16,47 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <iostream>
+#include <fstream>
+
 #ifndef SRC_LOGGER_H_
 #define SRC_LOGGER_H_
+
+// Standard log levels, ascending order of specificity.
+enum log_level_t {
+  SILLY,
+  DEBUG,
+  VERBOSE,
+  INFO,
+  WARN,
+  ERROR
+};
+
+const log_level_t default_log_level = DEBUG;
+
+class Logger {
+protected:
+  log_level_t threshold;
+  bool log_to_stdout;
+  bool log_to_file;
+  std::string log_filename;
+  std::ofstream log_file;
+
+public:
+  // Constructor sets an initial threshold
+  Logger(log_level_t threshold);
+  // Destructor closes an open log file
+  ~Logger();
+
+  void config(bool log_stdout, bool log_file, std::string fname);
+  void set_threshold(log_level_t level);
+  void log(log_level_t level, std::string msg);
+  void silly(std::string msg);
+  void debug(std::string msg);
+  void verbose(std::string msg);
+  void info(std::string msg);
+  void warn(std::string msg);
+  void error(std::string msg);
+};
 
 #endif  // SRC_LOGGER_H_
