5ebd724624fdc871a0ba60952319bb9cf42cf85b
Zack Mullaly <redwire@riseup.net>
2015-06-02 17:53:39 -0400

    Starting outline for code to test timers

---
+++ test/test_timers.cc
@@ -0,0 +1,39 @@
+/**
+ * Multiparty Off-the-Record Messaging library
+ * Copyright (C) 2014, eQualit.ie
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <utility>
+#include "contrib/gtest/include/gtest/gtesh.h"
+#include "src/session.h"
+#include "test/chat_mocker.h"
+
+class TimerTest : public ::testing::Test {};
+
+void test_fire_timer(ChatMocker chat_server, void (*timer)(void* arg), void* arg)
+{
+  
+}
+
+void test_stop_timer(ChatMocker chat_server, void (*timer)(void* arg), void* arg)
+{
+
+}
+
+TEST_F(TimerTest, test_timers)
+{
+  
+}
