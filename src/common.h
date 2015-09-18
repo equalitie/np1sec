89a4ac0e5597a62cb45c9598de54bfd564cdf649
Zack Mullaly <redwire@riseup.net>
2015-09-17 23:25:33 -0400

    Copied secure overwrite functions from libgcrypt

--- src/common.h
+++ src/common.h
@@ -77,6 +77,31 @@ const std::string c_subfield_delim(":"); // needed by ParticipantId defined in i
 // Global logger
 extern Logger logger;
 
+/**
+ * Quickly overwrite a piece of memory with some byte to prevent RAM inspection.
+ * @param {void*} _ptr - A pointer to the first byte of memory to overwrite
+ * @param {uint8_t} _set - The byte to write over the memory block with
+ * @param {size_t} _len - The number of bytes to write over
+ */
+#define wipememory2(_ptr,_set,_len) do { \
+        volatile char *_vptr=(volatile char *)(_ptr); \
+        size_t _vlen=(_len); \
+        unsigned char _vset=(_set); \
+        while(_vlen) { *_vptr=(_vset); _vptr++; _vlen--; } \
+    } while(0)
+
+/**
+ * Quickly overwrite a piece of memory a few times to prevent RAM inspection.
+ * @param {void*} _ptr - A pointer to the first byte of memory to overwrite
+ * @param {size_t} _len - The number of bytes to write over
+ */
+#define secure_wipe(_ptr,_len) do { \
+        wipememory2(_ptr,0xff,_len); \
+        wipememory2(_ptr,0xaa,_len); \
+        wipememory2(_ptr,0x55,_len); \
+        wipememory2(_ptr,0x00,_len); \
+    } while (0)
+
 } // namespace np1sec
 
 #endif // SRC_COMMON_H_
